ea41d8b1033e9079c28c636d2060682d7f084531
Farran-H <75946438+Farran-H@users.noreply.github.com>
2021-11-15 17:11:53 +0000

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator extends Reservation{
+    private double discountAP;
+    private double standardRes;
+
+    public Calculator(){
+        discountAP = 0.05;
+        standardRes = 1;
+    }
+}

281a4d57aa774beb28d288a8cc1381f24cd9c718
Farran-H <75946438+Farran-H@users.noreply.github.com>
2021-11-25 19:56:54 +0000

    Update Calculator.java
    I've further developed the calculator class to apply the 5% discount if the reservation is an advanced purchase.

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,33 @@
-public class Calculator extends Reservation{
-    private double discountAP;
-    private double standardRes;
+import java.util.ArrayList;
+
+public class Calculator extends Reservation {
+    private String reservationType;
 
     public Calculator(){
-        discountAP = 0.05;
-        standardRes = 1;
+        reservationType = "S";
+    }
+
+    //Accessor
+    public String getType(){
+        return reservationType;
+    }
+
+    //Mutator
+    public void setType(String type){
+        this.reservationType = type;
+    }
+
+    //Calculation
+    public double getDiscount(){
+        if (reservationType == "S"){
+            return totalCost;
+        } else if (reservationType == "AP"){
+            double temp;
+            temp = totalCost;
+            double discountedPrice = (temp * (5 / 100));
+            return discountedPrice;
+        } else {
+            return totalCost;
+        }
     }
 }

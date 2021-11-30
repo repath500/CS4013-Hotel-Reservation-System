cb01b5da0888a02adaed2550a3785df61c44be8e
Farran-H <75946438+Farran-H@users.noreply.github.com>
2021-11-30 21:17:43 +0000

    Update CsvRead.java
    Edited the l4hotel csv reader and added a new one to read the reservations.csv file

--- CsvRead.java
+++ CsvRead.java
@@ -5,7 +5,7 @@ public class CsvRead {
     private String[][] hotelInfo;
 
     // Reads the l4Hotels csv file and places it into a 2D array
-    public String[][] getHotelData() throws FileNotFoundException{
+    public String[][] getHotelData(){
         try {
             java.io.File csvFile = new java.io.File("l4Hotels.csv");
             // Sets the csv file as the input
@@ -25,5 +25,39 @@ public class CsvRead {
             e.printStackTrace();
         }
         return hotelInfo;
+    }
+    
+    // Reads in data from the reservations.csv file and places it into a 2D array
+    public String[][] getReservations() {
+        try {
+            java.io.File csvFile = new java.io.File("reservations.csv");
+            Scanner counter = new Scanner(csvFile);
+            /**
+             * First we create two strings, one is going to be used to count
+             * the amount of rows there will be in the array.
+             */
+            String line;
+            String x;
+            int y = 0;
+            while (counter.hasNext()){
+                x = counter.nextLine();
+                y++;
+            }
+            // Now we can use the y count to set the amount of rows.
+            counter.close();
+            Scanner input = new Scanner(csvFile);
+            this.reservations = new String[y][9];
+            // Then we reset the y count.
+            y = 0;
+            while(input.hasNext()){
+                line = input.nextLine();
+                this.reservations[y] = line.split(",");
+                y++;
+            }
+            input.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return reservations;
     }
 }
